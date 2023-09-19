22c1da03ccfcbbfb35f80cab7598639d0abe68f6
Victor Langat <provicml@gmail.com>
2023-09-19 11:23:19 +0300

    reverted base_model: test fix for file storage

--- models/base_model.py
+++ models/base_model.py
@@ -16,29 +16,44 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    # def __init__(self, *args, **kwargs):
+    #     """Instantiates a new model"""
+    #     self.id = str(uuid.uuid4())
+    #     self.created_at = datetime.now()
+    #     self.updated_at = datetime.now()
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "__class__":
-                    del kwargs['__class__']
+    #     if kwargs:
+    #         for key, value in kwargs.items():
+    #             if key == "created_at" or key == "updated_at":
+    #                 value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+    #             if key == "__class__":
+    #                 del kwargs['__class__']
+    #         self.__dict__.update(kwargs)
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        # cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        # return '[{}] ({}) {}'.format(cls, self.id, dict(self.__dict__))
-        instance_dict = dict(self.__dict__)
-        if '_sa_instance_state' in instance_dict:
-            del instance_dict['_sa_instance_state']
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, instance_dict)
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, dict(self.__dict__))
+        # instance_dict = dict(self.__dict__)
+        # if '_sa_instance_state' in instance_dict:
+        #     del instance_dict['_sa_instance_state']
+        # return "[{}] ({}) {}".format(
+        #     type(self).__name__, self.id, instance_dict)
 
     def __repr__(self):
         """return a string representaion"""
