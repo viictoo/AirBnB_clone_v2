0da59a42903ddfd05009e7a6cc540e874300fdb9
Victor Langat <provicml@gmail.com>
2023-09-20 18:31:04 +0300

    cleanup driver files

--- test_driver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export HBNB_MYSQL_USER='hbnb_test' \
-    HBNB_MYSQL_PWD='hbnb_test_pwd' \
-    HBNB_MYSQL_HOST='localhost' \
-    HBNB_MYSQL_DB='hbnb_test_db' \
-    HBNB_TYPE_STORAGE='db'
