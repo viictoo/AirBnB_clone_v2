3028da00e3a882306b557b28b6dc234803c90a1b
Victor Langat <provicml@gmail.com>
2023-10-04 23:32:53 +0300

    code fix: 0 errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,8 @@ sudo apt-get -y install -y nginx
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test/
 echo "RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW" > /data/web_static/releases/test/index.html
-sudo ln -nfs /data/web_static/releases/test/ /data/web_static/current
+sudo ln -nfs /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-str="\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
-sudo service restart nginx
+sudo service nginx restart
