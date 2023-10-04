ba805b2f8eada8c47beb5b97c221193b2393a22d
Victor Langat <provicml@gmail.com>
2023-10-04 22:53:58 +0300

    init commit: bare script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#  Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y install -y nginx
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+echo "RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW" > /data/web_static/releases/test/index.html
+sudo ln -nfs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+str="\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
+sudo service restart nginx
