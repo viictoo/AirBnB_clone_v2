4461723888b61a239a834e7823f06904c01821bd
Victor Langat <provicml@gmail.com>
2023-10-05 12:42:57 +0300

    test affiliate code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 
 sudo apt-get -y update
 sudo apt-get -y install -y nginx
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
 sudo echo "RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -nfs /data/web_static/releases/test /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data
 str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
