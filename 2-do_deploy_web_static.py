316e9ecd5750b467203f8076446aa2226891e757
Victor Langat <provicml@gmail.com>
2023-10-05 11:37:47 +0300

    used dot format

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """ distributes an archive to your web servers,
     using the function do_deploy
 """
-from fabric.api import env, hosts, run, put
+from fabric.api import env, run, put
 from os import path
 
 env.hosts = ['34.229.70.213', '3.89.160.146']
@@ -22,23 +22,25 @@ def do_deploy(archive_path):
     True if all operations have been done correctly, otherwise returns False
     """
     if not path.exists(archive_path):
+        print("False")
         return False
     try:
-        put(archive_path, '/tmp/')
 
         arch_tgz = archive_path.split('/')[-1]
         arch = arch_tgz.split('.')[0]
-        arch_dir = f'/data/web_static/releases/{arch}/'
+        arch_dir = '/data/web_static/releases/{}/'.format(arch)
 
-        run('sudo mkdir -p {}'.format(arch_dir))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(arch_tgz, arch_dir))
-        run('sudo rm /tmp/{}'.format(arch_tgz))
-        run('sudo mv {}web_static/* {}'.format(arch_dir, arch_dir))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(arch_dir))
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(arch_dir))
+        run('tar -xzf /tmp/{} -C {}'.format(arch_tgz, arch_dir))
+        run('rm /tmp/{}'.format(arch_tgz))
+        run('mv {}web_static/* {}'.format(arch_dir, arch_dir))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(arch_dir))
 
         print('New version deployed!')
         return True
 
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
