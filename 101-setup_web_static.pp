d7b9c0d6889963a9dcd6c810c8142a06dfeb5788
Victor Langat <provicml@gmail.com>
2023-10-06 02:24:27 +0300

    like my sushi

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,15 +1,15 @@
 # brute force method
-$str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}"
+
 exec { 'command':
 command  => "sudo apt-get -y update;
 sudo apt-get -y install -y nginx;
 mkdir -p /data/web_static/shared;
 mkdir -p /data/web_static/releases/test/;
-sudo echo 'Holberton School' | sudo tee /data/web_static/releases/test/index.html;
+sudo echo 'RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW' | sudo tee /data/web_static/releases/test/index.html;
 sudo ln -nfs /data/web_static/releases/test /data/web_static/current;
 sudo chown -R ubuntu:ubuntu /data;
-sudo sed -i '/^\tserver_name .*;/a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tindex index.html
-0-index.html 0-index.htm;\n\t}' /etc/nginx/sites-enabled/default;
+sudo sed -i '/^\tserver_name .*;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/curre
+nt/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}' /etc/nginx/sites-enabled/default;;
 sudo service nginx restart",
 provider => shell,
 }
