612bff2a41803c971e102f66f4b059d7cc6d237d
Victor Langat <provicml@gmail.com>
2023-10-05 14:40:39 +0300

    not related to code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
-# set up sequence
+#  Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get install -y nginx
-
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-
+sudo apt-get -y update
+sudo apt-get -y install -y nginx
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+sudo echo "RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -nfs /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
