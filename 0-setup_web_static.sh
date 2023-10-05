2fcf001e61b597abee4f756b0edb3689c028629f
Victor Langat <provicml@gmail.com>
2023-10-05 15:45:27 +0300

    test 0-index

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ mkdir -p /data/web_static/releases/test/
 sudo echo "RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -nfs /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tindex index.html;\n\t}"
+str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}"
 sudo sed -i "/^\tserver_name .*;/a ${str}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
