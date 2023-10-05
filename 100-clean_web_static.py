e37d575891742ec485a31e55a663b6a3797525fa
Victor Langat <provicml@gmail.com>
2023-10-05 17:45:17 +0300

    more checks

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""deletes out-of-date archives
-"""
-from fabric.api import local, run, env, task
+"""deletes out-of-date archives"""
+from fabric.api import local, env, run, task
 
 env.hosts = ["34.229.70.213", "3.89.160.146"]
 
@@ -19,11 +18,27 @@ def do_clean(number=0):
         and second most recent versions of your archive
     """
 
-    num = int(number)
-    rem = "cd /data/web_static/releases; ls | head -n -{} | xargs rm -rf"
-    if num == 0 or num == 1:
-        local("cd versions; ls | head -n -1 | xargs rm -rf")
-        run("cd /data/web_static/releases; ls | head -n -1 | xargs rm -rf")
-    else:
-        local("cd versions; ls | head -n -{} | xargs rm -rf".format(num))
-        run(rem.format(num))
+    try:
+        num = int(number)
+    except ValueError:
+        raise ValueError("Number argument must be a non-negative integer.")
+
+    if num < 0:
+        raise ValueError("Number argument must be a non-negative integer.")
+
+    # Local directory clean-up
+    local_versions_dir = "versions"
+    if num in (0, 1):
+        local("cd {} && ls | head -n -1 | xargs rm -rf"
+              .format(local_versions_dir))
+    elif num >= 2:
+        local("cd {} && ls | head -n -{} | xargs rm -rf"
+              .format(local_versions_dir, num))
+
+    # Remote directory clean-up
+    remote_versions_dir = "/data/web_static/releases"
+    try:
+        run("cd {} && ls | head -n -{} | xargs rm -rf"
+            .format(remote_versions_dir, num))
+    except Exception as e:
+        print("An error occurred while cleaning remote directory:", e)
