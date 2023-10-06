f26a98c5d426a1c809a8fe85f6fd092314c4c2ee
Victor Langat <provicml@gmail.com>
2023-10-06 11:28:55 +0300

    puppetteer

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,9 +7,15 @@ mkdir -p /data/web_static/shared;
 mkdir -p /data/web_static/releases/test/;
 sudo echo 'RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW' | sudo tee /data/web_static/releases/test/index.html;
 sudo ln -nfs /data/web_static/releases/test /data/web_static/current;
-sudo chown -R ubuntu:ubuntu /data;
-sudo sed -i '/^\tserver_name .*;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/curre
-nt/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}' /etc/nginx/sites-enabled/default;
-sudo service nginx restart",
+sudo chown -R ubuntu:ubuntu /data",
+provider => shell,
+}
+exec { 'add_lines':
+  command  => 'sudo sed -i "s|^\tserver_name .*;|server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}|" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+
+exec { 'command2':
+command  => 'sudo service nginx restart',
 provider => shell,
 }
