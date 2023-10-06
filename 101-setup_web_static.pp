2683baa2f392a3c0b3baa576d0e3e54c7d7169c1
Victor Langat <provicml@gmail.com>
2023-10-06 08:06:24 +0300

    3mons

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -9,7 +9,7 @@ sudo echo 'RIP ME OUT THE PLASTIC I BEEN ACTING BRAND NEW' | sudo tee /data/web_
 sudo ln -nfs /data/web_static/releases/test /data/web_static/current;
 sudo chown -R ubuntu:ubuntu /data;
 sudo sed -i '/^\tserver_name .*;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/curre
-nt/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}' /etc/nginx/sites-enabled/default;;
+nt/;\n\tindex index.html 0-index.html 0-index.htm;\n\t}' /etc/nginx/sites-enabled/default;
 sudo service nginx restart",
 provider => shell,
 }
