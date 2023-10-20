8b41e7fc4a57bc05ee56cba2739caa667c357dbf
Victor Langat <provicml@gmail.com>
2023-10-20 15:32:13 +0300

    edited state relationship

--- models/state.py
+++ models/state.py
@@ -11,24 +11,21 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
         cities = relationship(
             'City',
             backref='state',
             cascade="all, delete, delete-orphan")
     else:
-        name = ""
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """getter for cities related to state"""
             # from models.__init__ import storage
             # from models.city import City
             cit_list = []
-            city_stor = models.storage.all(City)
-            for c, val in city_stor.items():
+            city_stor = models.storage.all(City).values()
+            for val in city_stor:
                 if self.id == val.state_id:
                     cit_list.append(val)
             return cit_list
